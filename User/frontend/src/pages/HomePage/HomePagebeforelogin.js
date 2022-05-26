e279b95f4e03709d4a408d93ba68e9b5f643795e
sridewi25 <sridewisartika25@gmail.com>
2022-05-26 23:34:09 +0800

    FE User

--- User/frontend/src/pages/HomePage/HomePagebeforelogin.js
+++ User/frontend/src/pages/HomePage/HomePagebeforelogin.js
@@ -7,10 +7,40 @@ function HomePagebeforelogin() {
       <nav className="bg-image">
         <div className="container">
           <div className="row">
-          <div class="col-md-6 offset-md-3">
-            <h1>Welcome to Distro</h1>
-            <h2>Kami menyediakan segalaa jenis baju dengan harga yang bisa dijangkau semua kalangan masyarakat</h2>
-          </div>
+            <div className="col-md-8 offset-md-2">
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <h1 className="text-center text-style-h1">Welcome to Distro</h1>
+              <h4 className="text-center text-style-h4">
+                Kami menyediakan segala jenis baju dengan harga yang bisa
+                dijangkau semua kalangan masyarakat
+              </h4>
+              <br></br>
+              <h5 className="text-center text-style-h4">
+                Daftarkan Diri Anda dan Dapatkan Diskon Menarik
+              </h5>
+              <br></br>
+              <br></br>
+            </div>
+            <div class="col-3 offset-md-2">
+              <div className=" input-group flex-nowrap submit-btn input-align">
+                <button className="btn text-add">Login</button>
+              </div>
+              <br></br>
+            <br></br>
+            </div>
+            <div class="col-3 offset-md-2">
+              <div className=" input-group flex-nowrap submit-btn input-align">
+                <button className="btn text-add">Register</button>
+              </div>
+              <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            </div>
           </div>
         </div>
       </nav>
