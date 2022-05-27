e5ff2fa077e093609ae1930977d2ab650050b8a4
ronaldofernandus <ronaldofernandus10@gmail.com>
2022-05-28 00:27:09 +0700

    product

--- Admin/frontend/src/pages/Product/index.js
+++ Admin/frontend/src/pages/Product/index.js
@@ -1,9 +1,39 @@
 import React from "react";
+import { MdDeleteForever } from "react-icons/md";
+import { AiFillFileAdd } from "react-icons/ai";
+import { AiFillEdit } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const Product = () => {
   return (
     <>
-      <h1>Product Page</h1>
+      <div className="col-12 my-2">
+        <button type="button" className="btn btn-secondary">
+          <AiFillFileAdd></AiFillFileAdd>
+          <Link to="/add" className="add">
+            Add
+          </Link>
+        </button>
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Nama Produk</th>
+              <th>Deskripsi</th>
+              <th>Harga</th>
+              <th>Stok Barang</th>
+              <th>Expire</th>
+              <th>Berat Barang</th>
+              <th>Kategori</th>
+              <th>Brand</th>
+              <th>Kondisi</th>
+              <th>Total Terjual</th>
+              <th>Rating</th>
+              <th>View</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
     </>
   );
 };
