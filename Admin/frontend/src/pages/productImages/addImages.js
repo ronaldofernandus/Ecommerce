e99aa367bbf34d76ced739c9d3b3a9e24972287c
ronaldofernandus <ronaldofernandus10@gmail.com>
2022-05-31 19:53:28 +0700

    ..

--- Admin/frontend/src/pages/productImages/addImages.js
+++ Admin/frontend/src/pages/productImages/addImages.js
@@ -4,57 +4,67 @@ import { getImage, addImage } from "../../Axios/imageAxios";
 import { getProduct } from "../../Axios/productAxios";
 import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
+import { uploadImage } from "../../fetch/index";
 
 const AddImage = () => {
-  // const [prim_filename, setPrim_filename] = useState("");
-  // const [prim_filesize, setPrim_filesize] = useState("");
-  // const [prim_filetype, setPrim_filetype] = useState("");
-  // const [prim_primary, setPrim_primary] = useState("");
-  const [productId, setProductId] = useState("");
-
-  const [image, setImage] = useState("https://via.placeholder.com/150");
-  const [saveImage, setSaveImage] = useState(null);
-
   const { addImageResult } = useSelector((state) => state.imageReducers);
 
   const { getListProductResult, getListProductLoading, getListProductError } =
     useSelector((state) => state.productReducers);
 
+  const [image, setImage] = useState({
+    prim_filename: "",
+    prim_filesize: "",
+    prim_filetype: "",
+    prim_primary: "",
+  });
+
+  // const [image, setImage] = useState("https://via.placeholder.com/150");
+  // const [image, setImage] = useState({});
+  const [saveImage, setSaveImage] = useState(null);
+
+  const [productId, setProductId] = useState("");
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  // const onChangeHandler = (e) => {
-  //   let uploaded = e.target.files;
-  //   Array.from(uploaded).forEach((upload) => {
-
-  //     setImage(URL.createObjectURL(upload));
-  //   });
-
-  // };
+  const handleChange = (e) => {
+    console.log(e.target.files);
+    let uploaded = e.target.files;
+    Array.from(uploaded).forEach((upload) => {
+      setImage(URL.createObjectURL(upload));
+      setSaveImage(upload);
+    });
+  };
 
   // const onChangeHandler = (e) => {
   //   console.log(e.target.files[0]);
   //   let uploaded = e.target.files[0];
   //   setImage(URL.createObjectURL(uploaded));
-  //   saveImage(uploaded);
+  //   setSaveImage(uploaded);
   // };
+
   const addHandler = (event) => {
     // console.log("1. Mulai");
-    // dispatch(
-    //   addImage({
-    //     prim_filename: prim_filename,
-    //     prim_filesize: prim_filesize,
-    //     prim_filetype: prim_filetype,
-    //     prim_primary: prim_primary,
-    //     prim_primary: prim_primary,
-    //   })
-    // );
-    // Swal.fire({
-    //   icon: "success",
-    //   title: "Add Post Success!",
-    //   text: `You've successfully created an post!`,
-    // });
-    // navigate("/productImage");
+    dispatch(
+      addImage({
+        image: image,
+        productId: productId,
+      })
+    );
+    Swal.fire({
+      icon: "success",
+      title: "Add Post Success!",
+      text: `You've successfully created an post!`,
+    });
+    navigate("/productImage");
+  };
+
+  const submitPostHandler = () => {
+    const data = new FormData();
+    data.append("image", image);
+    uploadImage(data);
+    addHandler(image.name);
   };
 
   useEffect(() => {
@@ -107,22 +117,20 @@ const AddImage = () => {
             <label className="form-label" for="customFile">
               Gambar Product
             </label>
-            <div>
-              <img src={image} className="img-thumbnail" alt="" />
-            </div>
-
+            <img src={image} alt="" />
             <input
-              onChange={onChangeHandler}
               type="file"
+              onChange={handleChange}
+              // onChange={onChangeHandler}
               className="form-control"
               id="formFile"
               accept="image/*"
-              // multiple={true}
+              multiple={true}
             />
           </div>
 
           <button
-            // onClick={() => addHandler()}
+            onClick={() => submitPostHandler()}
             type="submit"
             className="btn btn-primary"
           >
